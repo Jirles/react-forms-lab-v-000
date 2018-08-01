54647af0c1694981f9501eaaf1a6c0a82b64e536
Emily Jirles <emily.jirles@gmail.com>
2018-08-01 13:26:20 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -21,6 +21,7 @@ class TwitterMessage extends React.Component {
       <div>
         <strong>Your message:</strong>
         <input type="text" id='twitterMessage' value={this.state.message} onChange={this.handleMessageChange}/>
+        <small>Characters Left: {this.state.charactersLeft}</small>
       </div>
     );
   }
