13d4d8dcb2cc0185a235d4d6ba0da9dc1505c4c5
Emily Jirles <emily.jirles@gmail.com>
2018-08-01 13:28:24 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,7 +16,7 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" value={this.state.username} onChange={}/>
+            <input id="username" name="username" type="text" value={this.state.username} onChange={} />
           </label>
         </div>
         <div>
